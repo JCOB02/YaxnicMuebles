fa861d05853f27eba9f04d55bd9774dc8993cdf2
Julio Olivera Baleon <julio2000olivera@hotmail.com>
2026-01-27 17:21:21 -0600

    chore: ignore eslint errors during build

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,10 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
       }],
     domains: ['picsum.photos'],  
-    }
+    },
+    eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
