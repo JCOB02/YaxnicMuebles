ca0265ef16f7003d98d943cc56ebaf2addf7a607
Julio Olivera Baleon <julio2000olivera@hotmail.com>
2026-01-27 23:31:11 -0600

    Fix Next.js 16 config (remove eslint option)

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      }],
-    domains: ['picsum.photos'],  
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "picsum.photos",
     },
-    eslint: {
-    ignoreDuringBuilds: true,
-  }
+    ],
+  },
+
+
 };
 
 export default nextConfig;
